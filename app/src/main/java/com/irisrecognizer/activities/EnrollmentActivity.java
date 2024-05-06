3f505b5c024187bb66fc61dbce237a7505c35a52
Sondos <sondosahmedabed@gmail.com>
2024-05-06 22:33:49 +0300

    Add EnrollmentActivity

---
+++ app/src/main/java/com/irisrecognizer/activities/EnrollmentActivity.java
@@ -0,0 +1,16 @@
+package com.irisrecognizer.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EnrollmentActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initialize();
+    }
+
+    private void initialize() {
+
+    }
+}
