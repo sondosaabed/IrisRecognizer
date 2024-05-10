b36c2494e3d025c0c62d465c19caaf3256a892e7
Sondos <sondosahmedabed@gmail.com>
2024-05-10 18:46:00 +0300

    Add Shared Prefrences (first_time) user

--- app/src/main/java/com/irisrecognizer/config/Preferences.java
+++ app/src/main/java/com/irisrecognizer/config/Preferences.java
@@ -1,19 +1,35 @@
 package com.irisrecognizer.config;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.preference.PreferenceManager;
+
 import com.irisrecognizer.activities.AuthenticationActivity;
-import com.irisrecognizer.activities.IntroductoryActivity;
 
 public class Preferences {
-    public static boolean isFirstTime(IntroductoryActivity authenticationActivity) {
-        return true;
+    private static final String FIRST_TIME = "is_first_time";
+    private static SharedPreferences preferences;
+    private static SharedPreferences.Editor editor;
+
+    /* These two methods are used to check if the user uses the app for the forst time */
+    public static boolean isFirstTime(Context context) {
+        preferences = getPreferences(context);
+        return preferences.getBoolean(FIRST_TIME, true);
     }
 
-    public static void setNotFirstTime(IntroductoryActivity authenticationActivity) {
+    public static void setNotFirstTime(Context context) {
+        preferences = getPreferences(context);
+        editor = preferences.edit();
+        editor.putBoolean(FIRST_TIME, false);
+        editor.apply();
     }
 
-    public static void setLanguageFromPreferences(AuthenticationActivity authenticationActivity) {
-    }
-
-    public static void change_language(AuthenticationActivity authenticationActivity) {
+    public static SharedPreferences getPreferences(Context context) {
+        if (preferences == null) {
+            preferences = PreferenceManager.getDefaultSharedPreferences(context);
+            editor = preferences.edit();
+        }
+        return preferences;
     }
 }
